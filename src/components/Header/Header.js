9fbe58a3b9ae2f2bb52017a7d7bf829a1688a9d6
ZachWagner1 <zwag14@yahoo.com>
2020-10-03 19:51:09 -0500

    components added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../Navigation/Navigation'
+
+function Header() {
+    return (
+        <div className="Header">
+            HEADER
+            <Navigation />
+        </div>
+    )
+}
+
+export default Header
